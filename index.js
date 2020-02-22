bc66e0f14ebd0df505506d6f002daefec2b723b0
Hanna Lee <mani21na@gmail.com>
2020-02-22 03:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const = names["Ada", "Brendan", "Ali"];
-
 function writeCards(names, event) {
   for (let i = 0; i < names.length; i++) {
-    console.log('Thank you, ${names[i]}, for the wonderful $[event] gift!');
+    console.log(`Thank you, ${names[i]}, for the wonderful $[event] gift!`);
   }
 }
