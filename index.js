e86504033d96664f7ef09557a51179ec5b7b8546
Hanna Lee <mani21na@gmail.com>
2020-02-22 03:47:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let write = [];
+  for (let i = 0; i < names.length; i++) {
+    write.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return write;
+}
+
+function countDown(num){
+  while (num > -1){
+    console.log(num);
+    num-- ;
+  }
+}
